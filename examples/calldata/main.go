7c609ea0ba4218323b42f6e440ae70c5b3f8159c
Martin Holst Swende <martin@swende.se>
2019-11-20 09:33:49 +0100

    add some printouts before actual measurement run

--- examples/calldata/main.go
+++ examples/calldata/main.go
@@ -14,12 +14,12 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the goevmlab library. If not, see <http://www.gnu.org/licenses/>.
 
-
 package main
 
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/holiman/goevmlab/ops"
 	"math/big"
 	"os"
@@ -47,18 +47,17 @@ func runit() error {
 
 	aAddr := common.HexToAddress("0xff0a")
 
-    // Calling contract: Call contract B in a loop
-    a.Op(ops.PC)         // Push 0
-	a.Op(ops.DUP1)       // outsize = 0, on next iteration we use the return value of CALL 
+	// Calling contract: Call contract B in a loop
+	a.Op(ops.PC)         // Push 0
+	a.Op(ops.DUP1)       // outsize = 0, on next iteration we use the return value of CALL
 	dest := a.Jumpdest() // Loop Head
 	a.Op(ops.DUP2)       // outoffset = 0
-    a.Push(1305700)      // insize = 1305700
+	a.Push(1305700)      // insize = 1305700
 	a.Op(ops.DUP2)       // inoffset = 0
-    a.Push(0xdeadbeef)   // Push target address, alternatively we could call an empty contract here
-    a.Op(ops.GAS)        // Pass along all gas
-    a.Op(ops.STATICCALL)
-	a.Jump(dest)         // Jump back
-
+	a.Push(0xdeadbeef)   // Push target address, alternatively we could call an empty contract here
+	a.Op(ops.GAS)        // Pass along all gas
+	a.Op(ops.STATICCALL)
+	a.Jump(dest) // Jump back
 
 	alloc := make(core.GenesisAlloc)
 	alloc[aAddr] = core.GenesisAccount{
@@ -108,15 +107,48 @@ func runit() error {
 			PetersburgBlock:     new(big.Int),
 			IstanbulBlock:       new(big.Int),
 		},
+		EVMConfig: vm.Config{
+			Debug:  true,
+			Tracer: &dumbTracer{},
+		},
 	}
+	// Run with tracing
+	_, _, err = runtime.Call(aAddr, nil, &runtimeConfig)
 	// Diagnose it
+	runtimeConfig.EVMConfig = vm.Config{}
 	t0 := time.Now()
 	_, _, err = runtime.Call(aAddr, nil, &runtimeConfig)
 	t1 := time.Since(t0)
 	fmt.Printf("Time elapsed: %v\n", t1)
-	t0 = time.Now()
-	_, _, err = runtime.Call(aAddr, nil, &runtimeConfig)
-	t1 = time.Since(t0)
-	fmt.Printf("Time elapsed: %v\n", t1)
 	return err
+}
+
+type dumbTracer struct {
+	counter uint64
+}
+
+func (d *dumbTracer) CaptureStart(from common.Address, to common.Address, call bool, input []byte, gas uint64, value *big.Int) error {
+	fmt.Printf("captureStart\n")
+	fmt.Printf("	from: %v\n", from.Hex())
+	fmt.Printf("	to: %v\n", to.Hex())
+	return nil
+}
+
+func (d *dumbTracer) CaptureState(env *vm.EVM, pc uint64, op vm.OpCode, gas, cost uint64, memory *vm.Memory, stack *vm.Stack, contract *vm.Contract, depth int, err error) error {
+	fmt.Printf("pc %d op %v gas %d cost %d\n", pc, op, gas, cost)
+	if op == vm.STATICCALL {
+		d.counter++
+	}
+	return nil
+}
+
+func (d *dumbTracer) CaptureFault(env *vm.EVM, pc uint64, op vm.OpCode, gas, cost uint64, memory *vm.Memory, stack *vm.Stack, contract *vm.Contract, depth int, err error) error {
+	fmt.Printf("CaptureFault\n")
+	return nil
+}
+
+func (d *dumbTracer) CaptureEnd(output []byte, gasUsed uint64, t time.Duration, err error) error {
+	fmt.Printf("\nCaptureEnd\n")
+	fmt.Printf("Counter: %d\n", d.counter)
+	return nil
 }
