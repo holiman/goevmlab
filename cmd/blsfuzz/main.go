628cc257e0e9fd09e1b10f26608fd7af120cb25b
Marius van der Wijden <m.vanderwijden@live.de>
2020-05-28 18:16:49 +0200

    cmd: added blsfuzz command

---
+++ cmd/blsfuzz/main.go
@@ -0,0 +1,64 @@
+// Copyright 2020 Martin Holst Swende, Marius van der Wijden
+// This file is part of the go-evmlab library.
+//
+// The library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the goevmlab library. If not, see <http://www.gnu.org/licenses/>.
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/urfave/cli.v1"
+
+	"github.com/holiman/goevmlab/common"
+	"github.com/holiman/goevmlab/fuzzing"
+)
+
+func initApp() *cli.App {
+	app := cli.NewApp()
+	app.Name = filepath.Base(os.Args[0])
+	app.Author = "Martin Holst Swende"
+	app.Usage = "Generator for bls12-381 (state-)tests"
+	app.Flags = []cli.Flag{
+		common.GethFlag,
+		common.ParityFlag,
+		common.NethermindFlag,
+		common.AlethFlag,
+		common.ThreadFlag,
+		common.LocationFlag,
+	}
+	app.Action = startFuzzer
+	return app
+}
+
+var app = initApp()
+
+func main() {
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func startFuzzer(c *cli.Context) error {
+	generator := func() *fuzzing.GstMaker {
+		base := fuzzing.GenerateBLS()
+		target := base.GetDestination()
+		base.SetCode(target, fuzzing.RandCallBLS())
+		return base
+	}
+	return common.ExecuteFuzzer(c, generator, "blstest")
+}
---
+++ fuzzing/bls12381.go
@@ -0,0 +1,101 @@
+// Copyright 2020 Martin Holst Swende, Marius van der Wijden
+// This file is part of the goevmlab library.
+//
+// The library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the goevmlab library. If not, see <http://www.gnu.org/licenses/>.
+
+package fuzzing
+
+import (
+	"math/big"
+	"math/rand"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/holiman/goevmlab/ops"
+	"github.com/holiman/goevmlab/program"
+)
+
+var precompilesBLS = []int{
+	10, 11, 12, 13, 14, 15, 16, 17, 18,
+}
+
+var inputSizes = []int{
+	256, 256, 160, 512, 512, 288, 384, 64, 128,
+}
+
+var ouputSizes = []int{
+	128, 128, 128, 256, 256, 256, 32, 128, 256,
+}
+
+func GenerateBLS() *GstMaker {
+	gst := basicStateTest()
+	// Add a contract which calls BLS
+	dest := common.HexToAddress("0x0000ca1100b1a7e")
+	gst.AddAccount(dest, GenesisAccount{
+		Code:    RandCallBLS(),
+		Balance: new(big.Int),
+		Storage: make(map[common.Hash]common.Hash),
+	})
+	// The transaction
+	{
+		tx := &stTransaction{
+			// 8M gaslimit
+			GasLimit:   []uint64{8000000},
+			Nonce:      0,
+			Value:      []string{randHex(4)},
+			Data:       []string{randHex(100)},
+			GasPrice:   big.NewInt(0x01),
+			To:         dest.Hex(),
+			PrivateKey: hexutil.MustDecode("0x45a915e4d060149eb4365960e6a7a45f334393093061116b197e3240065ff2d8"),
+		}
+		gst.SetTx(tx)
+	}
+	return gst
+}
+
+func RandCallBLS() []byte {
+	p := program.NewProgram()
+	precompile := rand.Int31n(int32(len(precompilesBLS)))
+	sizeIn := inputSizes[precompile]
+	if sizeIn == 160 || sizeIn == 288 || sizeIn == 384 {
+		sizeIn = sizeIn * rand.Intn(1000)
+	}
+	data := randomBLSArgs(sizeIn)
+	p.Mstore(data, 0)
+	memInFn := func() (offset, size interface{}) {
+		offset, size = 0, sizeIn
+		return
+	}
+	sizeOut := ouputSizes[precompile]
+	memOutFn := func() (offset, size interface{}) {
+		offset, size = 0, sizeOut
+		return
+	}
+	addrGen := func() interface{} {
+		return precompilesBLS[precompile]
+	}
+	p2 := RandCall(GasRandomizer(), addrGen, ValueRandomizer(), memInFn, memOutFn)
+	p.AddAll(p2)
+	// pop the ret value
+	p.Op(ops.POP)
+	// Store the output in some slot, to make sure the stateroot changes
+	p.MemToStorage(0, sizeOut, 0)
+	return p.Bytecode()
+}
+
+func randomBLSArgs(inputSize int) []byte {
+	data := make([]byte, inputSize)
+	rand.Read(data)
+	return data
+}
