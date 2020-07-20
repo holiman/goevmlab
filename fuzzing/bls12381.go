d23a788723a83070c201ad2477e6237cd8ee194b
Marius van der Wijden <m.vanderwijden@live.de>
2020-07-20 13:07:36 +0200

    fuzzing: added custom mutator and smarter point generation logic

--- fuzzing/bls12381.go
+++ fuzzing/bls12381.go
@@ -24,20 +24,25 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto/bls12381"
 	"github.com/holiman/goevmlab/ops"
 	"github.com/holiman/goevmlab/program"
 )
 
-var precompilesBLS = []int{
-	10, 11, 12, 13, 14, 15, 16, 17, 18,
+var modulo, _ = big.NewInt(0).SetString("0x1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffaaab", 0)
+
+type blsPrec struct {
+	addr    int
+	newData func(iv []byte, config MutationConfig) []byte
+	outsize int
 }
 
-var inputSizes = []int{
-	256, 256, 160, 512, 512, 288, 384, 64, 128,
-}
-
-var ouputSizes = []int{
-	128, 128, 128, 256, 256, 256, 32, 128, 256,
+var precompilesBLS = []blsPrec{
+	blsPrec{10, NewG1Add, 0}, // G1Add
+	blsPrec{11, NewG1Mul, 0}, // G1Mul
+	blsPrec{12, NewG1Exp, 0}, // G1Exp
+	blsPrec{13, NewG2Add, 0}, // G2Add
+	blsPrec{14, NewG2Mul, 0}, // G2Add
 }
 
 func GenerateBLS() *GstMaker {
@@ -69,24 +74,26 @@ func GenerateBLS() *GstMaker {
 func RandCallBLS() []byte {
 	p := program.NewProgram()
 	offset := 0
-	for precompile := range precompilesBLS {
-		sizeIn := inputSizes[precompile]
+	var iv []byte
+	config := new(MutationConfig)
+	for _, precompile := range precompilesBLS {
+		data := precompile.newData(iv, *config)
+		sizeIn := len(data)
 		if sizeIn == 160 || sizeIn == 288 || sizeIn == 384 {
 			sizeIn = sizeIn * rand.Intn(1000)
 		}
-		data := randomBLSArgs(sizeIn)
 		p.Mstore(data, 0)
 		memInFn := func() (offset, size interface{}) {
 			offset, size = 0, sizeIn
 			return
 		}
-		sizeOut := ouputSizes[precompile]
+		sizeOut := precompile.outsize
 		memOutFn := func() (offset, size interface{}) {
 			offset, size = 0, sizeOut
 			return
 		}
 		addrGen := func() interface{} {
-			return precompilesBLS[precompile]
+			return precompile.addr
 		}
 		p2 := RandCall(GasRandomizer(), addrGen, ValueRandomizer(), memInFn, memOutFn)
 		p.AddAll(p2)
@@ -131,4 +138,81 @@ func ReadBLSCorpus() error {
 		BLSCorpus[i] = input
 	}
 	return nil
+}
+
+func NewG1Add(iv []byte, config MutationConfig) []byte {
+	a := NewG1Point(iv, config)
+	b := NewG1Point(iv, config)
+	return append(a, b...)
+}
+
+func NewG1Mul(iv []byte, config MutationConfig) []byte {
+	a := NewG1Point(iv, config)
+	mul := make([]byte, 32)
+	rand.Read(mul)
+	return append(a, mul...)
+}
+
+func NewG1Exp(iv []byte, config MutationConfig) []byte {
+	i := rand.Int31n(140)
+	var res []byte
+	for k := 0; k < int(i); k++ {
+		input := NewG1Mul(iv, config)
+		res = append(res, input...)
+	}
+	return res
+}
+
+func NewG2Add(iv []byte, config MutationConfig) []byte {
+	a := NewG2Point(iv, config)
+	b := NewG2Point(iv, config)
+	return append(a, b...)
+}
+
+func NewG2Mul(iv []byte, config MutationConfig) []byte {
+	a := NewG2Point(iv, config)
+	mul := make([]byte, 32)
+	rand.Read(mul)
+	return append(a, mul...)
+}
+
+// sanitizeMutate maps arbitrary input to a valid field element on the curve
+func sanitizeMutate(data []byte) []byte {
+	fe := big.NewInt(0).SetBytes(data)
+	if fe.Cmp(modulo) == 1 {
+		fe = fe.Mod(fe, modulo)
+	}
+	buf := make([]byte, 48)
+	copy(buf[48-len(fe.Bytes()):], fe.Bytes())
+	return buf
+}
+
+func NewFieldElement(iv []byte, config MutationConfig) []byte {
+	re := Mutate(iv, config)
+	return sanitizeMutate(re)
+}
+
+func NewG1Point(iv []byte, config MutationConfig) []byte {
+	a := NewFieldElement(iv, config)
+	// Initialize G1
+	g := bls12381.NewG1()
+	b, err := g.MapToCurve(a)
+	if err != nil {
+		panic(err)
+	}
+	return g.EncodePoint(b)
+}
+
+func NewG2Point(iv []byte, config MutationConfig) []byte {
+	a := NewFieldElement(iv, config)
+	b := NewFieldElement(a, config)
+	x := append(a, b...)
+	// Initialize G2
+	g := bls12381.NewG2()
+	// Compute mapping
+	res, err := g.MapToCurve(x)
+	if err != nil {
+		panic(err)
+	}
+	return g.EncodePoint(res)
 }
