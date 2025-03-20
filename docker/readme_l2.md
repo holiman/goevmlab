47a4a30ab0567df9ed8837c5a60ce84a7ed5b484
Martin Holst Swende <martin@swende.se>
2025-03-20 10:42:30 +0100

    basic l2: [bor-geth] and [arb-op-geth] fuzzing

---
+++ docker/readme_l2.md
@@ -0,0 +1,45 @@
+This is a dockerfile containing various L2 VMs, plus go-evmlab itself.
+The evm binaries are available as ENV vars:
+
+- "regular" vms
+  - `$GETH_BIN`=/gethvm - The regular normal geth.
+  - `$OP_BIN`=/opvm - optimism fork of geth
+  - `$ARB_BIN`=/arbvm - offchain labs arbitrum fork of geth
+
+- "non-regular" vms, where the consensus rules are changed.
+  - `$BORGETH_BIN`=/borgovm - A bor-flavoured geth, which is same as regular geth but with a few tiny tweaks to make it execute statetests similarly.
+  - `$BOR_BIN`=/borvm - the vm from https://github.com/maticnetwork/bor
+    - This is based on an older geth, this needs it's own shim: `--bor` or `--borbatch`. (All the other use `--geth`/`--gethbatch`)
+
+
+## Generating reference output
+
+TOOD
+
+## Checkslow
+
+## Run a test against all clients
+
+```
+docker run -it -v /home/user/workspace/tests/fuzztmp:/fuzztmp --entrypoint bash holiman/omnifuzz
+
+$ runtest $FUZZ_CLIENTS /fuzztmp/
+```
+
+
+## Fuzzing
+
+If you want to do fuzzing, you should ensure that the directory where tests are saved is mounted outside the docker container
+
+```
+docker run -it -v /home/user/fuzzing:/fuzztmp
+
+$ generic-fuzzer --outdir=/fuzztmp  --fork=Cancun $VMS_REGULAR
+```
+or
+```
+docker run -it -v /home/user/fuzzing:/fuzztmp
+
+$ generic-fuzzer --outdir=/fuzztmp  --fork=Cancun $VMS_BOR
+```
+
---
+++ evms/bor.go
@@ -0,0 +1,187 @@
+// Copyright 2019 Martin Holst Swende
+// This file is part of the goevmlab library.
+//
+// The library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the goevmlab library. If not, see <http://www.gnu.org/licenses/>.
+
+package evms
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"time"
+
+	"github.com/ethereum/go-ethereum/log"
+)
+
+// BorEVM is s Evm-interface wrapper around the bor `evm` binary, based on go-ethereum.
+type BorEVM struct {
+	path string
+	name string // in case multiple instances are used
+
+	// Some metrics
+	stats *VmStat
+}
+
+func NewBorEVM(path string, name string) *BorEVM {
+	return &BorEVM{
+		path:  path,
+		name:  name,
+		stats: &VmStat{},
+	}
+}
+
+func (evm *BorEVM) Instance(int) Evm {
+	return evm
+}
+
+func (evm *BorEVM) Name() string {
+	return evm.name
+}
+
+// GetStateRoot runs the test and returns the stateroot
+// This currently only works for non-filled statetests. TODO: make it work even if the
+// test is filled. Either by getting the whole trace, or adding stateroot to exec std output
+// even in success-case
+func (evm *BorEVM) GetStateRoot(path string) (root, command string, err error) {
+	// In this mode, we can run it without tracing
+	cmd := exec.Command(evm.path, "statetest", path)
+	data, err := cmd.Output()
+	if err != nil {
+		return "", cmd.String(), err
+	}
+	root, err = evm.ParseStateRoot(data)
+	if err != nil {
+		log.Error("Failed to find stateroot", "vm", evm.Name(), "cmd", cmd.String())
+		return "", cmd.String(), err
+	}
+	return root, cmd.String(), err
+}
+
+// ParseStateRoot reads geth's stateroot from the combined output.
+func (evm *BorEVM) ParseStateRoot(data []byte) (string, error) {
+	start := bytes.Index(data, []byte(`"stateRoot": "`))
+	end := start + 14 + 66
+	if start == -1 || end >= len(data) {
+		return "", fmt.Errorf("%v: no stateroot found", evm.Name())
+	}
+	return string(data[start+14 : end]), nil
+}
+
+// RunStateTest implements the Evm interface
+func (evm *BorEVM) RunStateTest(path string, out io.Writer, speedTest bool) (*tracingResult, error) {
+	var (
+		t0     = time.Now()
+		stderr io.ReadCloser
+		err    error
+		cmd    = exec.Command(evm.path, "--json", "--noreturndata", "--nomemory", "statetest", path)
+	)
+	if speedTest {
+		cmd = exec.Command(evm.path, "--nomemory", "--noreturndata", "--nostack", "statetest", path)
+	}
+	if stderr, err = cmd.StderrPipe(); err != nil {
+		return &tracingResult{Cmd: cmd.String()}, err
+	}
+	if err = cmd.Start(); err != nil {
+		return &tracingResult{Cmd: cmd.String()}, err
+	}
+	// copy everything to the given writer
+	evm.Copy(out, stderr)
+	_, _ = io.ReadAll(stderr)
+	err = cmd.Wait()
+	// release resources
+	duration, slow := evm.stats.TraceDone(t0)
+
+	return &tracingResult{
+		Slow:     slow,
+		ExecTime: duration,
+		Cmd:      cmd.String(),
+	}, err
+}
+
+func (vm *BorEVM) Close() {
+}
+
+// Copy reads from the reader, does some geth-specific filtering and
+// outputs items onto the channel
+func (evm *BorEVM) Copy(out io.Writer, input io.Reader) {
+	evm.copyUntilEnd(out, input)
+}
+
+// copyUntilEnd reads from the reader, does some geth-specific filtering and
+// outputs items onto the channel
+func (evm *BorEVM) copyUntilEnd(out io.Writer, input io.Reader) stateRoot {
+	scanner := NewJsonlScanner("geth", input, os.Stderr)
+	defer scanner.Release()
+	var stateRoot stateRoot
+	// When geth encounters an error, it may already have spat out the info, prematurely.
+	// We need to merge it back to one item
+	// https://github.com/ethereum/go-ethereum/pull/23970#issuecomment-979851712
+	var prev *opLog
+	var yield = func(current *opLog) {
+		if prev == nil {
+			prev = current
+			return
+		}
+		data := CustomMarshal(prev)
+		if _, err := out.Write(append(data, '\n')); err != nil {
+			fmt.Fprintf(os.Stderr, "Error writing to out: %v\n", err)
+		}
+		if current == nil { // final flush
+			return
+		}
+		if prev.Pc == current.Pc && prev.Depth == current.Depth && prev.FunctionDepth == current.FunctionDepth {
+			// Yup, that happened here. Set the error and continue
+			prev = nil
+		} else {
+			prev = current
+		}
+	}
+	for {
+		var elem opLog
+		if err := scanner.Next(&elem); err != nil {
+			break
+		}
+		// If we have a stateroot, we're done
+		if len(elem.StateRoot1) != 0 {
+			stateRoot.StateRoot = elem.StateRoot1
+			break
+		}
+		// If the output cannot be marshalled, all fields will be blanks.
+		// We can detect that through 'depth', which should never be less than 1
+		// for any actual opcode
+		if elem.Depth == 0 {
+			continue
+		}
+		// When geth encounters end of code, it continues anyway, on a 'virtual' STOP.
+		// In order to handle that, we need to drop all STOP opcodes.
+		if elem.Op == 0x0 {
+			continue
+		}
+		yield(&elem)
+	}
+	yield(nil)
+	root, _ := json.Marshal(stateRoot)
+	if _, err := out.Write(append(root, '\n')); err != nil {
+		fmt.Fprintf(os.Stderr, "Error writing to out: %v\n", err)
+	}
+	return stateRoot
+}
+
+func (evm *BorEVM) Stats() []any {
+	return evm.stats.Stats()
+}
