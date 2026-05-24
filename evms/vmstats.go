e2220ff8ae6d73e6bf0b4066feb7ab6d34f4cd56
Sahil Sojitra <sahilsojitra4555@gmail.com>
2026-05-24 11:21:36 +0530

    evms: make longestTracingTime concurrency-safe

--- evms/vmstats.go
+++ evms/vmstats.go
@@ -1,23 +1,17 @@
 package evms
 
 import (
-	"sync/atomic"
 	"time"
 
 	"github.com/holiman/goevmlab/utils"
-)
-
-const (
-	slowTestWarmupRuns = 500
+	"sync/atomic"
 )
 
 type VMStat struct {
 	// Some metrics
-	tracingSpeedWMA     utils.SlidingAverage
-	lastTracingTime     atomic.Uint64
-	shortestTracingTime atomic.Uint64
-	longestTracingTime  atomic.Uint64
-	numExecs            atomic.Uint64
+	tracingSpeedWMA    utils.SlidingAverage
+	longestTracingTime atomic.Int64
+	numExecs           atomic.Uint64
 }
 
 // TraceDone marks the tracing speed metric, and returns 'true' if the test is
@@ -26,13 +20,12 @@ func (stat *VMStat) TraceDone(start time.Time) (time.Duration, bool) {
 	numexecs := stat.numExecs.Add(1)
 	duration := time.Since(start)
 	stat.tracingSpeedWMA.Add(int(duration))
-
-	durationNs := uint64(duration)
-	stat.lastTracingTime.Store(durationNs)
-	updateMinDuration(&stat.shortestTracingTime, durationNs)
-	if updateMaxDuration(&stat.longestTracingTime, durationNs) {
-		// Don't count the first runs, let the VM warm up.
-		if numexecs > slowTestWarmupRuns {
+	// This is not strictly correct - two racing updates may interleave and
+	// one may overwrite the other. That's fine, it's just a bit of stats.
+	if duration > time.Duration(stat.longestTracingTime.Load()) {
+		stat.longestTracingTime.Store(int64(duration))
+		// Don't count the first 500 runs, let it accumulate.
+		if numexecs > 500 {
 			return duration, true
 		}
 	}
@@ -42,34 +35,8 @@ func (stat *VMStat) TraceDone(start time.Time) (time.Duration, bool) {
 func (stat *VMStat) Stats() []any {
 	return []any{
 		"execSpeed", time.Duration(stat.tracingSpeedWMA.Avg()).Round(100 * time.Microsecond),
-		"last", time.Duration(stat.lastTracingTime.Load()).Round(100 * time.Microsecond),
-		"shortest", time.Duration(stat.shortestTracingTime.Load()).Round(100 * time.Microsecond),
 		"longest", time.Duration(stat.longestTracingTime.Load()),
 		"count", stat.numExecs.Load(),
-	}
-}
-
-func updateMinDuration(field *atomic.Uint64, duration uint64) {
-	for {
-		current := field.Load()
-		if current != 0 && current <= duration {
-			return
-		}
-		if field.CompareAndSwap(current, duration) {
-			return
-		}
-	}
-}
-
-func updateMaxDuration(field *atomic.Uint64, duration uint64) bool {
-	for {
-		current := field.Load()
-		if current >= duration {
-			return false
-		}
-		if field.CompareAndSwap(current, duration) {
-			return true
-		}
 	}
 }
 
